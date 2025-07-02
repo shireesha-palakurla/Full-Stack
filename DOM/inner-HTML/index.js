005b5f5f059f6f4fbe8bf0e2be223016f736d3ac
shireesha-palakurla <pshireesha0706@gmail.com>
2025-07-02 21:10:47 +0530

    Create index.js
    Simple alert demonstration- alert will pop up immediately when page loads. Arrow function that updates the h1 element- Finds the first h1 element on page and  Changes its inner HTML to demonstration text.

---
+++ DOM/inner-HTML/index.js
@@ -0,0 +1,8 @@
+// Simple alert demonstration
+        alert("Click here!!!");
+        
+        // Arrow function demonstrating DOM manipulation
+        const alpha = () => {
+            var a = document.querySelector("h1");
+            a.innerHTML = " Warm Welcome!!";
+        };
