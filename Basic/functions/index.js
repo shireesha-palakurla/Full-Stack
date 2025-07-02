e57ed8f2f84bb48641114e9a03391e6bc6c8f408
shireesha-palakurla <pshireesha0706@gmail.com>
2025-07-02 14:14:36 +0530

    Update index.js

--- Basic/functions/index.js
+++ Basic/functions/index.js
@@ -1,4 +1,30 @@
-// Function expression with arrow function syntax
+// Function to alert a message
+function a(){
+var a=prompt("enter first number");
+a=parseInt(a);
+var b=prompt("enter second number");
+ b=parseInt(b);
+var c=a+b;
+alert(c);
+}
+
+//functions and types
+// Function declaration
+function ccc(a,b){
+    return a+b;
+}
+var c=ccc(4,6);
+console.log(c);
+
+ // Function expression
+ var beta=function(a,b){
+     return a+b;
+ }
+ var c=beta(4,6);
+ console.log(c);
+ 
+ 
+ // Function expression with arrow function syntax
  // This is a concise way to write functions in JavaScript
  var beta=(a,b)=> console.log(a+b)
  var c=beta(4,6)
