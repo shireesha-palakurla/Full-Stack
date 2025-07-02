ad98f69a7d759403282a17a37fbe28ee61d3e466
shireesha-palakurla <pshireesha0706@gmail.com>
2025-07-02 14:18:29 +0530

    Create index.js
    Can also run in https://www.onlinegdb.com/  for better results.

---
+++ Basic/loops/index.js
@@ -0,0 +1,29 @@
+//loops
+ // for loop
+ for(let i=1;i<=5;i++){
+    console.log(i);
+ }
+ //while loop
+ var i=1;
+ while(i<=5){
+    console.log(i);
+    i++;
+ }
+
+ //do while loop
+ var i=1;
+ do{
+    console.log(i);
+    i++;
+ }while(i<=5);
+
+ //for of
+ for(let i of [1,2,3,4])
+ {
+    console.log(i);
+ }
+ //for in
+ var obj={name:"siri",dept:"it"};
+ for(let i in obj){
+     console.log(i,obj[i]);
+ }
