b4f35f762bd596f0d92a058e3bd274f7343fc3a0
shireesha-palakurla <pshireesha0706@gmail.com>
2025-07-02 14:21:25 +0530

    Create index.js

---
+++ Basic/constructor/index.js
@@ -0,0 +1,10 @@
+// Object creation using constructor function
+// Constructor function is a special type of function that is used to create objects
+ //constructor
+ function Student(name,age)
+ {
+     this.name=name;
+     this.age=age;
+ }
+ const st1=new Student("ashwitha",20);
+ console.log(st1.name);
