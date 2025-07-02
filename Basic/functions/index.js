1f8dca4ecdac4ac086da73cc863e97c6cbaf9bcc
shireesha-palakurla <pshireesha0706@gmail.com>
2025-07-02 14:12:33 +0530

    Create index.js

---
+++ Basic/functions/index.js
@@ -0,0 +1,13 @@
+// Function expression with arrow function syntax
+ // This is a concise way to write functions in JavaScript
+ var beta=(a,b)=> console.log(a+b)
+ var c=beta(4,6)
+ 
+ 
+ // Function expression with arrow function syntax
+ // This is a concise way to write functions in JavaScript
+ var peta=(a,b)=>{
+     return a+b;
+ }
+ var c=peta(4,6);
+ console.log(c);
